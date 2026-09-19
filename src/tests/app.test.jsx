14613932e204956f1f47a77a484055512a2e5305
ananya <trehanananya9876@gmail.com>
2026-09-19 22:12:32 +0530

    test added

---
+++ src/tests/app.test.jsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import { expect, test } from "vitest";
+import App from "../App";
+
+test("renders App", () => {
+  render(<App />);
+
+  expect(screen.getByText("This is React App")).toBeInTheDocument();
+});
