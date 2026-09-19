df50bd016a2a0dcaf4fe5d2b4b4dff359f06ff11
ananya <trehanananya9876@gmail.com>
2026-09-19 19:11:32 +0530

    react project initialized

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <h1>This is React App</h1>
+      </section>
+     
+    </>
+  )
+}
+
+export default App
